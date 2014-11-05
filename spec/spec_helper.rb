58cb588ba242fc6c47fac67eb28ed4067dc35654
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-05 11:05:13 +0900

    test: add vim_current_buffer() helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,3 +17,7 @@ def enable_social_snippet
   vim.add_plugin(plugin_path, 'plugin/social_snippet.vim')
 end
 
+def vim_current_buffer
+  vim.command('echomsg join(getline(1, "$"), "\n")').strip
+end
+
