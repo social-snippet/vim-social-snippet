6f99f2c1fb16cf91444546c96d70b028fefbf752
Hiroyuki Sano <sh19910711@gmail.com>
2014-11-05 11:04:11 +0900

    test: use vimrunner

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require "vimrunner"
+require "vimrunner/rspec"
+
+Vimrunner::RSpec.configure do |config|
+  config.reuse_server = false
+
+  config.start_vim do
+    vim = Vimrunner.start
+
+    vim
+  end
+end
+
